d12e4a8ad635fecf96d1591abfa537fecace045e
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-13 16:28:29 -0300

    Export and test color

--- src/aestiva/__init__.py
+++ src/aestiva/__init__.py
@@ -1,3 +1,9 @@
 """Facilities for working with colors."""
 
 __version__ = "0.1.0"
+
+
+__all__ = ["Color"]
+
+
+from ._color import Color
--- tests/test_aestiva.py
+++ tests/test_aestiva.py
@@ -1,5 +1,12 @@
-from aestiva import __version__
+from aestiva import Color, __version__
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_color():
+    color = Color.frombytes(127.5, 127.5, 127.5)
+    assert color.red == 0.5
+    assert color.green == 0.5
+    assert color.blue == 0.5
