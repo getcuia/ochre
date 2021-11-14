84bf194af8465bd777f8a48afc0ef5f853a520b1
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-14 10:14:49 -0300

    Add tests to RGB to LUV conversions

--- tests/test_colorsys.py
+++ tests/test_colorsys.py
@@ -14,4 +14,12 @@ def test_rgb_to_xyz_roundtrip(r: float, g: float, b: float) -> None:
     """Test that rgb_to_xyz and xyz_to_rgb are inverses."""
     x, y, z = colorsys.rgb_to_xyz(r, g, b)
     r2, g2, b2 = colorsys.xyz_to_rgb(x, y, z)
-    assert (r2, g2, b2) == pytest.approx((r, g, b), rel=3e-10, abs=7e-7)
+    assert (r2, g2, b2) == pytest.approx((r, g, b), rel=1e-9, abs=1e-6)
+
+
+@given(r=between_0_and_1, g=between_0_and_1, b=between_0_and_1)
+def test_rgb_to_luv_roundtrip(r: float, g: float, b: float) -> None:
+    """Test that rgb_to_luv and luv_to_rgb are inverses."""
+    x, y, z = colorsys.rgb_to_luv(r, g, b)
+    r2, g2, b2 = colorsys.luv_to_rgb(x, y, z)
+    assert (r2, g2, b2) == pytest.approx((r, g, b), rel=1e-9, abs=1e-6)
