ae115e8709eabc59fd55c50a5ab29058c4444444
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-14 09:31:49 -0300

    Support an RGB to XYZ roundtrip

--- src/melanin/colorsys.py
+++ src/melanin/colorsys.py
@@ -10,9 +10,9 @@ Examples
 
 This module provides conversions between RGB and XYZ coordinates:
 
->>> colorsys.rgb_to_xyz(0.2, 0.4, 0.4)
-(1.0, 0.1, 0.1)
->>> colorsys.xyz_to_rgb(1.0, 0.1, 0.1)
+>>> colorsys.rgb_to_xyz(0.2, 0.4, 0.4)  # doctest: +NUMBER
+(0.09, 0.11, 0.14)
+>>> colorsys.xyz_to_rgb(0.09, 0.11, 0.14)  # doctest: +NUMBER
 (0.2, 0.4, 0.4)
 
 The standard coversions are still available:
@@ -26,7 +26,6 @@ The standard coversions are still available:
 
 from __future__ import annotations
 
-
 from colorsys import (
     hls_to_rgb,
     hsv_to_rgb,
@@ -36,18 +35,21 @@ from colorsys import (
     yiq_to_rgb,
 )
 
-
 __all__ = [
+    # Re-exported from colorsys
     "hls_to_rgb",
     "hsv_to_rgb",
     "rgb_to_hls",
     "rgb_to_hsv",
     "rgb_to_yiq",
     "yiq_to_rgb",
+    # New functions
+    "rgb_to_xyz",
+    "xyz_to_rgb",
 ]
 
 
-def rgb_to_xyz(r, g, b) -> tuple[float, float, float]:
+def rgb_to_xyz(r: float, g: float, b: float) -> tuple[float, float, float]:
     """Convert the color from RGB coordinates to XYZ coordinates."""
     if r > 0.04045:
         r = ((r + 0.055) / 1.055) ** 2.4
@@ -66,3 +68,43 @@ def rgb_to_xyz(r, g, b) -> tuple[float, float, float]:
     y = 0.2126 * r + 0.7152 * g + 0.0722 * b
     z = 0.0193 * r + 0.1192 * g + 0.9505 * b
     return x, y, z
+
+
+def xyz_to_rgb(x: float, y: float, z: float) -> tuple[float, float, float]:
+    """Convert the color from XYZ coordinates to RGB coordinates."""
+    # We're using a higher precision matrix here see
+    # <https://en.wikipedia.org/wiki/SRGB#sYCC_extended-gamut_transformation>
+    red = 3.2406254 * x - 1.537208 * y - 0.4986286 * z
+    green = -0.9689307 * x + 1.8757561 * y + 0.0415175 * z
+    blue = 0.0557101 * x - 0.2040211 * y + 1.0569959 * z
+
+    if red > 0.0031308:
+        red = 1.055 * (red ** (1 / 2.4)) - 0.055
+    else:
+        red = 12.92 * red
+    if green > 0.0031308:
+        green = 1.055 * (green ** (1 / 2.4)) - 0.055
+    else:
+        green = 12.92 * green
+    if blue > 0.0031308:
+        blue = 1.055 * (blue ** (1 / 2.4)) - 0.055
+    else:
+        blue = 12.92 * blue
+
+    return _clamp(red), _clamp(green), _clamp(blue)
+
+
+def _clamp(value: float, min_value: float = 0.0, max_value: float = 1.0) -> float:
+    """
+    Clamp a value to a range.
+
+    Examples
+    --------
+    >>> _clamp(-0.5)
+    0.0
+    >>> _clamp(0.5)
+    0.5
+    >>> _clamp(1.5)
+    1.0
+    """
+    return max(min_value, min(value, max_value))
