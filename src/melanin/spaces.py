a66c7a91312962ad293250a7731a3f615e66c43a
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-14 20:17:44 -0300

    Implement Ansi256 colors

--- src/melanin/spaces.py
+++ src/melanin/spaces.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import math
 from abc import ABC, abstractmethod
-from typing import Optional, Text
+from typing import Iterable, Optional, Text, TypeVar
 
 from . import colorsys, web
 
@@ -22,6 +22,12 @@ class Color(ABC):
     @abstractmethod
     def web_color(self) -> WebColor:
         """Return the color as a WebColor object."""
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def ansi256(self) -> Ansi256:
+        """Return the color as an Ansi256 object."""
         raise NotImplementedError()
 
     @property
@@ -45,9 +51,9 @@ class Color(ABC):
         """Return the index of the color as an hexadecimal integer."""
         return self.hex.integer
 
-    def __eq__(self, other: Color) -> bool:
+    def __eq__(self, other) -> bool:
         """Return True if the colors are equal."""
-        return _dist_rgb(self, other) < 7e-3
+        return _dist_rgb(self.rgb, other.rgb) < 7e-3
 
 
 class Hex(Color):
@@ -70,6 +76,11 @@ class Hex(Color):
     def web_color(self) -> WebColor:
         """Return the color as a WebColor object."""
         return self.rgb.web_color
+
+    @property
+    def ansi256(self) -> Ansi256:
+        """Return the color as an Ansi256 object."""
+        return self.rgb.ansi256
 
     @property
     def rgb(self) -> RGB:
@@ -109,6 +120,11 @@ class WebColor(Color):
         return self
 
     @property
+    def ansi256(self) -> Ansi256:
+        """Return the color as an Ansi256 object."""
+        return self.rgb.ansi256
+
+    @property
     def rgb(self) -> RGB:
         """Return the color as an RGB object."""
         return self.hex.rgb
@@ -121,6 +137,65 @@ class WebColor(Color):
     def __repr__(self) -> Text:
         """Return a string representation of the color."""
         return f"WebColor({self.name!r})"
+
+
+class Ansi256(Color):
+    """A color represented by an integer between 0 and 255."""
+
+    code: int
+
+    ANSI8 = ["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+
+    def __init__(self, code: int) -> None:
+        """Initialize an ANSI color."""
+        self.code = code
+
+    @property
+    def hex(self) -> Hex:
+        """Return the color as an Hex object."""
+        return self.rgb.hex
+
+    @property
+    def web_color(self) -> WebColor:
+        """Return the color as a WebColor object."""
+        return self.rgb.web_color
+
+    @property
+    def ansi256(self) -> Ansi256:
+        """Return the color as an Ansi256 object."""
+        return self
+
+    @property
+    def rgb(self) -> RGB:
+        """Return the color as an RGB object."""
+        if self.code < 16:
+            name = self.ANSI8[self.code]
+            if self.code > 7:
+                name = f"light{name}"
+            return WebColor(name).rgb
+        if self.code < 232:
+            red_i = (self.code - 16) // 36
+            green_i = ((self.code - 16) % 36) // 6
+            blue_i = (self.code - 16) % 6
+
+            red = 55 + red_i * 40 if red_i > 0 else 0
+            green = 55 + green_i * 40 if green_i > 0 else 0
+            blue = 55 + blue_i * 40 if blue_i > 0 else 0
+
+            return RGB(red / 255, green / 255, blue / 255)
+        if self.code < 256:
+            value = (self.code - 232) * 10 + 8
+            return RGB(value / 255, value / 255, value / 255)
+        raise ValueError(f"Invalid ANSI code {self.code}")
+
+    @property
+    def hcl(self) -> HCL:
+        """Return the color as an HCL object."""
+        return self.rgb.hcl
+
+    def __repr__(self) -> Text:
+        """Return a string representation of the color."""
+        return f"Ansi256({self.code})"
 
 
 class RGB(Color):
@@ -147,16 +222,12 @@ class RGB(Color):
     @property
     def web_color(self) -> WebColor:
         """Return the color as a WebColor object."""
-        minimal_name: Optional[Text] = None
-        minimal_distance = math.inf
+        return _closest_color_rgb(self, map(WebColor, web.colors.keys()))
 
-        for name, hexstr in web.colors.items():
-            distance = _dist_rgb(self, Hex(hexstr))
-            if distance < minimal_distance:
-                minimal_name = name
-                minimal_distance = distance
-        assert minimal_name is not None
-        return WebColor(minimal_name)
+    @property
+    def ansi256(self) -> Ansi256:
+        """Return the color as an Ansi256 object."""
+        return _closest_color_rgb(self, map(Ansi256, range(256)))
 
     @property
     def rgb(self) -> RGB:
@@ -197,6 +268,11 @@ class HCL(Color):
         return self.rgb.web_color
 
     @property
+    def ansi256(self) -> Ansi256:
+        """Return the color as an Ansi256 object."""
+        return self.rgb.ansi256
+
+    @property
     def rgb(self) -> RGB:
         """Return the color as an RGB object."""
         return RGB(*colorsys.hcl_to_rgb(self.hue, self.chroma, self.luminance))
@@ -211,12 +287,30 @@ class HCL(Color):
         return f"HCL({self.hue}, {self.chroma}, {self.luminance})"
 
 
-def _dist_rgb(color1: Color, color2: Color) -> float:
+def _dist_rgb(color1: RGB, color2: RGB) -> float:
     """Return the distance between two RGB colors."""
-    rgb1 = color1.rgb
-    rgb2 = color2.rgb
     return math.sqrt(
-        (rgb1.red - rgb2.red) ** 2
-        + (rgb1.green - rgb2.green) ** 2
-        + (rgb1.blue - rgb2.blue) ** 2
+        (color1.red - color2.red) ** 2
+        + (color1.green - color2.green) ** 2
+        + (color1.blue - color2.blue) ** 2
     )
+
+
+C = TypeVar("C", bound=Color)
+
+
+def _closest_color_rgb(color: RGB, colors: Iterable[C]) -> C:
+    """Return the closest color to the given color."""
+    minimal_color: Optional[C] = None
+    minimal_distance = math.inf
+
+    for other in colors:
+        distance = _dist_rgb(color, other.rgb)
+        if distance < minimal_distance:
+            minimal_color = other
+            minimal_distance = distance
+
+    assert minimal_color is not None
+    return minimal_color
+
+    # return min(colors, key=lambda c: _dist_rgb(color, c))
