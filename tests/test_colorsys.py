ffd754b5a90192eac76ca862e489cd1df7a41ce7
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-15 11:11:01 -0300

    Test Ansi256 color formulas

--- tests/test_colorsys.py
+++ tests/test_colorsys.py
@@ -31,3 +31,22 @@ def test_rgb_to_hcl_roundtrip(r: float, g: float, b: float) -> None:
     h, c, ell = colorsys.rgb_to_hcl(r, g, b)
     r2, g2, b2 = colorsys.hcl_to_rgb(h, c, ell)
     assert (r2, g2, b2) == pytest.approx((r, g, b), abs=9e-7)
+
+
+@given(code=st.integers(min_value=16, max_value=231))
+def test_ansi256_cube_formula(code: int):
+    """Test correctness of the cube formula for ANSI 256 colors."""
+    r_i = (code - 16) // 36
+    g_i = ((code - 16) % 36) // 6
+    b_i = (code - 16) % 6
+    r = (55 + r_i * 40) / 255 if r_i > 0 else 0
+    g = (55 + g_i * 40) / 255 if g_i > 0 else 0
+    b = (55 + b_i * 40) / 255 if b_i > 0 else 0
+    assert colorsys.ansi256_to_rgb(code) == pytest.approx((r, g, b), abs=9e-7)
+
+
+@given(code=st.integers(min_value=232, max_value=255))
+def test_ansi256_grayscale_formula(code: int):
+    """Test correctness of the grayscale formula for ANSI 256 colors."""
+    r = g = b = ((code - 232) * 10 + 8) / 255
+    assert colorsys.ansi256_to_rgb(code) == pytest.approx((r, g, b), abs=9e-7)
