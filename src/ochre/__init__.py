f641b6aea64ce09a9ad4eb705caa6d8b4271633e
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-22 16:34:49 -0300

    Bump version

--- src/ochre/__init__.py
+++ src/ochre/__init__.py
@@ -1,7 +1,7 @@
 """Facilities for working with colors."""
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 __all__ = ["Ansi256", "Color", "HCL", "Hex", "RGB", "WebColor"]
--- tests/test_ochre.py
+++ tests/test_ochre.py
@@ -5,4 +5,4 @@ from ochre import __version__
 
 def test_version():
     """Test version."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
