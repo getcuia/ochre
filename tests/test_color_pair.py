8e666f12025f629003fdd1ec6b5cde106789f681
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-12-17 12:23:59 -0300

    Ensure color pairs are immutable

--- tests/test_color_pair.py
+++ tests/test_color_pair.py
@@ -1,5 +1,8 @@
 """Tests for color pairs."""
 
+from dataclasses import FrozenInstanceError
+
+import pytest
 from hypothesis import given
 
 from ochre import ColorPair
@@ -26,6 +29,29 @@ def test_can_create_color_pairs(
 
     assert color_pair.foreground == foreground
     assert color_pair.background == background
+
+
+@given(
+    rf=between_0_and_1,
+    gf=between_0_and_1,
+    bf=between_0_and_1,
+    rb=between_0_and_1,
+    gb=between_0_and_1,
+    bb=between_0_and_1,
+)
+def test_color_pairs_are_immutable(
+    rf: float, gf: float, bf: float, rb: float, gb: float, bb: float
+):
+    """Test that color pairs are immutable."""
+    foreground = RGB(rf, gf, bf)
+    background = RGB(rb, gb, bb)
+    color_pair = ColorPair(foreground, background)
+
+    with pytest.raises(FrozenInstanceError):
+        color_pair.foreground = background
+
+    with pytest.raises(FrozenInstanceError):
+        color_pair.background = foreground
 
 
 @given(
